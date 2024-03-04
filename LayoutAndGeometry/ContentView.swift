c4f1d2ed627cb53e162b8adfb130e5e68565e2ec
Leo Barnett <leo.e.barnett@icloud.com>
2024-03-04 15:10:04 +0100

    final version

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -10,27 +10,36 @@ import SwiftUI
 
 
 struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+    
+    
+
     var body: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: 0) {
-                ForEach(1..<20) { num in
-                    Text("Number \(num)")
-                        .font(.largeTitle)
-                        .padding()
-                        .background(.red)
-                        .clipShape(.capsule)
-                        .frame(width: 200, height: 200)
-                        .visualEffect { content, proxy in
-                            content
-                                .rotation3DEffect(
-                                    .degrees(-proxy.frame(in: .global).minX) / 8,
-                                    axis: (x: 0, y: 1, z: 0))
-                        }
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
+                    GeometryReader { proxy in
+                        let minY = proxy.frame(in: .global).minY
+                        let offset = minY - fullView.size.height / 2
+                        let opacity = proxy.frame(in: .global).minY / 200
+                        let scaleFactor = max(1 - offset / fullView.size.height, 0.5)
+                        let hue = min(1, (proxy.frame(in: .global).maxY / fullView.frame(in: .global).height))
+                        
+                        Text("Row \(index) : \(opacity.formatted())")
+                            .font(.title.bold())
+                            .frame(maxWidth: .infinity)
+                            .foregroundStyle(.secondary)
+                            .background(Color(hue: hue, saturation: 0.8, brightness: 1))
+                            .clipShape(.capsule)
+                            .opacity(opacity)
+                            .scaleEffect(scaleFactor)
+                            .rotation3DEffect(.degrees(offset) / 5, axis: (x: 0, y: 1, z: 0))
+                    }
+                    .frame(height: 40)
                 }
             }
-            .scrollTargetLayout()
         }
-        .scrollTargetBehavior(.viewAligned)
+        .background(.black)
     }
 }
 
